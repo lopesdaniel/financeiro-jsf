9124634712781c178e643e527d21c7fe0d5978f1
lopesdaniel <daniellopesdonascimento@gmail.com>
2018-02-19 17:44:26 -0300

    Primeiro commit

---
+++ src/com/blogspot/danieldeveloper/model/Pessoa.java
@@ -0,0 +1,29 @@
+package com.blogspot.danieldeveloper.model;
+
+import java.io.Serializable;
+
+public class Pessoa implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String nome;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+
+	
+	
+}
