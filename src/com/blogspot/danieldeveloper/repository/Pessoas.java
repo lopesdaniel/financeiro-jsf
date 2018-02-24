aec32ff1cb15f0774202aca953b902affe3397cc
lopesdaniel <daniellopesdonascimento@gmail.com>
2018-02-24 00:48:12 -0300

    Criado repositório Pessoas

---
+++ src/com/blogspot/danieldeveloper/repository/Pessoas.java
@@ -0,0 +1,31 @@
+package com.blogspot.danieldeveloper.repository;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import com.blogspot.danieldeveloper.model.Pessoa;
+
+public class Pessoas implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private EntityManager manager;
+	
+	public Pessoas(EntityManager manager) {
+		this.manager = manager;
+	}
+	
+	public Pessoa porId(Long id) {
+		return manager.find(Pessoa.class, id);
+	}
+	
+	public List<Pessoa> todas(){
+		TypedQuery<Pessoa> query = manager.createQuery("from Pessoa", Pessoa.class);
+		
+		return query.getResultList();
+	}
+	
+}
